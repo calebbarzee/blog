3c61577a9be1e40e7792486a6e2a3597758c797b
Caleb Barzee <barzeec@gmail.com>
2023-05-20 10:23:35 -0600

    Content rendering properly

--- src/utilities/get_md.ts
+++ src/utilities/get_md.ts
@@ -1,4 +1,6 @@
 import showdown from "showdown";
+
+
 const converter = new showdown.Converter();
 
 // This function fetches from a filepath a Markdown file and returns the content as HTML in a string.
@@ -8,7 +10,7 @@ const converter = new showdown.Converter();
       const response = await fetch(filepath);
       const data = await response.text();
       const html = converter.makeHtml(data);
-      console.log(html);
+      // console.log(html);
       return html;
    } catch (err) {
      console.error('There was an error fetching the Markdown file:', err);
@@ -18,8 +20,12 @@ const converter = new showdown.Converter();
  async function parseMarkdown(filepath: string): Promise<{title: string, content: string}> {
    const html = await fetchMarkdown("/2023/may/19.md")
      const title: string = html.match(/<h1(.*?)<\/h1>/)[0];
-     const content: string = html.replace(/<h1(.*?)<\/h1>/, "");
+     let content: string = html.replace(/<h1(.*?)<\/h1>/, "");
+    content = content.replace(/<pre><code>[\s\S]*?<\/code><\/pre>/g, `<span onload="hljs.highlightAll()">$&</span>`);
+      // console.log(content);
      return {title, content};
  }
 
 export { parseMarkdown };
+
+// for use in future syntax highlighting hljs.highlightAuto(block, ["json", "ts", "js", "html", "css", "swift", "py", "go", "cpp"]);
